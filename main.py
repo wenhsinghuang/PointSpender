78f9ad35ca2efae81c174750efbb736ed208e983
whhuang4 <whhuang4@illinois.edu>
2023-02-02 01:29:36 -0600

    implement core code

---
+++ main.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import csv
+from datetime import datetime
+from utils.models import Transaction
+from utils.point_spender import point_spender
+
+def parse_timestamp(timestamp_str: str) -> datetime:
+    return datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%SZ')
+
+def main(amount_to_spend: int) -> None:
+    """
+    Process I/O and data pre-processing.
+
+    Parameters:
+    amount_to_spend (int): The amount of points to spend.
+
+    Returns:
+    None.
+    """
+    csv_file_path = os.path.join(os.getcwd(), 'transactions.csv')
+    if not os.path.exists(csv_file_path):
+        print(f'Error: The transactions.csv file was not found in the current working directory ({os.getcwd()})')
+        sys.exit(1)
+    
+    if not os.access(csv_file_path, os.R_OK):
+        # file is not readable
+        print('Error: file is not readable')
+        sys.exit(1)
+        
+    transactions = []
+    with open(csv_file_path) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader) # skip header
+        for row in reader:
+            payer, points, timestamp = row
+            transactions.append(Transaction(payer, int(points), parse_timestamp(timestamp)))
+    
+    transactions.sort(key=lambda x:x.timestamp)
+    # through this API, we can change only main() to adapt different data format.
+    balances = point_spender(transactions, amount_to_spend)
+    print(balances)
+
+if __name__ == '__main__':
+    # argument check
+    if len(sys.argv) != 2:
+        print('Error: Incorrect number of arguments. Please specify the amount to spend.')
+        sys.exit(1)
+
+    try:
+        amount_to_spend = int(sys.argv[1])
+    except ValueError:
+        print('Error: Invalid argument type. Please enter a valid integer.')
+        sys.exit(1)
+    
+    if amount_to_spend < 0:
+        print('Error: The amount to spend should not be a negative number.')
+        sys.exit(1)
+    
+    main(amount_to_spend)
---
+++ utils/point_spender.py
@@ -0,0 +1,50 @@
+
+from typing import List, Dict
+from utils.models import Transaction
+
+
+def point_spender(transactions: List[Transaction], amount_to_spend: int) -> Dict[str, int]:
+    """
+    API for to calculate balances of each payer after spend.
+
+    Parameters:
+    transactions (List[Transaction]): The initial transactions record.
+    amount_to_spend (int): The amount of points to spend.
+
+    Returns:
+    balances (Dict[str, int]): balances of each payer after spend
+    """
+    # sort transactions by timestamp
+    transactions.sort(key=lambda x: x.timestamp)
+    
+    # check if amount_to_spend over total points the user have.
+    if sum(transactions, key=lambda x:x.points) < amount_to_spend:
+        raise Exception("Error: The amount to spend should not exceed total balance.")
+
+    # calculate temporary transactions that have handled the history spending.
+    for i in range(len(transactions)):
+        if transactions[i].points < 0:
+            for j in range(0,i):
+                pay = min(transactions[j].points, abs(transactions[i].points))
+                transactions[j].points -= pay
+                transactions[i].points += pay
+                if transactions[i].points == 0:
+                    break
+            if transactions[i].points < 0:
+                raise Exception("Error: one history transaction exceed total balance at its timestamp.")
+    
+    # spend points based on rules
+    for i in range(len(transactions)):
+        pay = min(transactions[i].points, amount_to_spend)
+        transactions[i].points -= pay
+        amount_to_spend -= pay
+        if amount_to_spend == 0:
+            break
+
+    balances = {}
+    for i in range(len(transactions)):
+        if transactions[i].payer not in balances:
+            balances[transactions[i].payer] = 0
+        balances[transactions[i].payer] += transactions[i].points
+    
+    return balances
